78dfa74bb1efc0a9969b8b4b96d15e286396b187
suxiaolin <dear.lin@live.com>
2018-01-13 16:49:25 +0800

    update

---
+++ cpp/ourfunc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void simon(int); // function protype for simon()
+
+int main()
+{
+    using namespace std;
+    simon(3); // call the simon() function
+    cout << "Pick an integer: ";
+    int count;
+    cin >> count;
+    simon(count); // call it agin
+    cout << "Done!" << endl;
+
+    return 0;
+}
+
+void simon(int n) // define the simon() function
+{
+    using namespace std;
+    cout << "Simon says touch your toes " << n << " times." << endl;
+    // void function don't need return statements
+}
